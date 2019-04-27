bd63bad41eda3157112131042cda8b1c07d74ae5
Wallas Faria <wallasfaria@hotmail.com>
2019-04-27 02:35:08 -0300

    Exercício closures

---
+++ 10-closures/exercicio-01.js
@@ -0,0 +1,15 @@
+numeros = [20, 345, 100, 3251, 32, 453, 684, 2521, 425, 94];
+
+mostreOs(
+  numerosMenoresQue(100),
+  numerosEntre(250).e(500),
+  numerosMaioresQue(1000)
+).doArray(numeros)
+
+/*
+Dever mostrar no console:
+
+numeros menores que: 20, 32, 94
+numeros entre 250 e 500: 345, 453, 425
+numeros maiores que: 3251, 2521
+*/
