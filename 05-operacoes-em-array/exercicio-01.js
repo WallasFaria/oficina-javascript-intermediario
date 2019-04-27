61c818f9ca0c3873cd8419dff84d261f7c951a94
Wallas Faria <wallasfaria@hotmail.com>
2019-04-26 21:25:38 -0300

    add exercícios de arrays

---
+++ 05-operacoes-em-array/exercicio-01.js
@@ -0,0 +1,9 @@
+/*
+Percorra lista de clientes e mostre no console
+apenas o nome e a quantidade de tags de cada um
+
+Exemplo: Daniels Obrien (3)
+usar: forEach e template string
+*/
+
+const clientes = require('../_dados/clientes.json')
---
+++ 05-operacoes-em-array/exercicio-06.js
@@ -0,0 +1,8 @@
+/*
+Ferifique se há algum cliente com mais de 4 tags
+usar: some
+*/
+
+const clientes = require('../_dados/clientes.json')
+
+// escreva seu código na linha abaixo:
